395cf1407c38d0a1f91c9ce422844be625068c20
Precontation <github.reliably383@passmail.com>
2026-09-20 15:17:59 -0700

    Added pirate speak for some reason

--- main.py
+++ main.py
@@ -9,17 +9,34 @@ dotenv.load_dotenv()
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='/', intents=intents)
 
+# Pirate stuff 🔥
+try:
+    from arrr import translate # pyright: ignore[reportMissingImports]
+    PIRATE_AVAILABLE = True
+    print("Pirate available!")
+except ImportError:
+    PIRATE_AVAILABLE = False
+    print("Pirate unavailable... :(")
+
 @bot.event
 async def on_ready():
     await bot.tree.sync()
-    print(f'Bot is logged in and ready!')
+    print(f'Logged in and ready!')
 
-@bot.tree.command(name='repeat', description='Repeat exactly what you just typed')
+@bot.tree.command(name='repeat', description='Repeat exactly what you typed')
 @discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True) # Allow use in guilds
 @discord.app_commands.allowed_installs(guilds=False, users=True) # But you can't install it TO a guild
 async def on_message(interaction: discord.Interaction, text: str):
     await interaction.response.send_message(text)
 
-bot.run(str(os.environ['DISCORD_BOT_TOKEN']))
+if PIRATE_AVAILABLE:
+    @bot.tree.command(name='repeat-as-a-pirate', description='idk why i made this')
+    @discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True) # Allow use in guilds
+    @discord.app_commands.allowed_installs(guilds=False, users=True) # But you can't install it TO a guild
+    async def on_message(interaction: discord.Interaction, text: str):
+        pirate = translate(text)
+        await interaction.response.send_message(pirate)
+
+bot.run(os.environ['DISCORD_BOT_TOKEN'])
 
 
