a2a75e5ea16abdfb6684ef23f0b96a0839338dcc
Precontation <github.reliably383@passmail.com>
2026-09-20 03:40:56 -0700

    improve

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A quick lil discord bot to bypass EVIL DICTATOR CENSORSHIP AAAAAA
+A quick lil discord bot to bypass EVIL DICTATOR CENSORSHIP RAAAAAA (cough cough Xeolt)
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ dotenv.load_dotenv()
 
 # Discord stuff
 intents = discord.Intents.default()
-intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
@@ -16,11 +15,10 @@ async def on_ready():
     print(f'Bot is logged in and ready!')
 
 @bot.tree.command(name='repeat', description='Repeat exactly what you just typed')
-@discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-@discord.app_commands.allowed_installs(guilds=True, users=True)
+@discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True) # Allow use in guilds
+@discord.app_commands.allowed_installs(guilds=False, users=True) # But you can't install it TO a guild
 async def on_message(interaction: discord.Interaction, text: str):
-    await interaction.response.send_message(text);
-
+    await interaction.response.send_message(text)
 
 bot.run(str(os.environ['DISCORD_BOT_TOKEN']))
 
